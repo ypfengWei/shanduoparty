826960fd5da11e30a47e7a6fc5b8da94dbb38b1b
fanshixin <136219541@qq.com>
2018-05-31 19:54:06 +0800

    添加群组功能 Signed-off-by: fanshixin <136219541@qq.com>

---
+++ src/main/java/com/shanduo/party/entity/UserGroup.java
@@ -0,0 +1,95 @@
+package com.shanduo.party.entity;
+
+import java.util.Date;
+
+public class UserGroup {
+    private String id;
+
+    private Integer userId;
+
+    private String groupId;
+
+    private Integer count;
+
+    private String groupType;
+
+    private String remarks;
+
+    private Date createDate;
+
+    private Date updateDate;
+
+    private String delFlag;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId == null ? null : groupId.trim();
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getGroupType() {
+        return groupType;
+    }
+
+    public void setGroupType(String groupType) {
+        this.groupType = groupType == null ? null : groupType.trim();
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks == null ? null : remarks.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag == null ? null : delFlag.trim();
+    }
+}
